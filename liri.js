e3caf8c8110b413ed4e5deace8779ce80aeed974
Brent Woodcock <brentwoodcock@gmail.com>
2017-02-08 14:00:06 -0600

    basic spotify functionality added

--- liri.js
+++ liri.js
@@ -25,30 +25,51 @@ var selectCommand = function(command, request) {
 }
 
 var getTweets = function(username) {
-	console.log('twitter requested');
 
 	var client = new Twitter (keys.twitterKeys)
 
-	if (username == undefined) {
+	if (username === undefined) {
 		username = 'realDonaldTrump';
 	}
 
 	var params = {screen_name: username};
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if (error){
-			console.log(error);
-		} else {
-			for (var i = 0; i < tweets.length; i++) {
-				console.log(tweets[i].text);
-				console.log(tweets[i].created_at);
-				console.log();
-			}
+	client.get('statuses/user_timeline', params, function(err, tweets, response) {
+		if (err){
+			console.log(err);
+			return;
+		}
+
+		for (var i = 0; i < tweets.length; i++) {
+			console.log(tweets[i].text);
+			console.log(tweets[i].created_at);
+			console.log();
 		}
 	})
 }
 
 var getSong = function(songName) {
-	console.log(songName + ' requested');
+
+	if (songName === undefined) {
+		songName = 'The Sign';
+	}
+
+	spotify.search({ type: 'track', query: songName }, function(err, data) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		var results = data.tracks.items;
+
+		for (var i = 0; i < results.length; i++) {
+			console.log('Result ' + (i + 1));
+			console.log('Song Name: ' + results[i].name);
+			console.log('Artist: ' + results[i].artists[0].name);
+			console.log('Preview URL: ' + results[i].preview_url);
+			console.log('Album: ' + results[i].album.name);
+			console.log();
+		}
+	})
 }
 
 var getMovie = function(movieName) {
